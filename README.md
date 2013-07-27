a3c9ea5a8accfd98d6d6c7777404dbefe843dc8c
caster <caster@brain.(none)>
2013-07-27 09:22:59 -0700

    Initial Commit: Added dirwatch.py, small README.md description

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ dirwatch
 ========
 
 Directory Watcher
+----------------
+
+Directory watcher will watch the specified directory for any updated files and automatically compile files based upon their file extension.
+Currently the supported extensions are: .tex, .less, .sass
+
+
---
+++ dirwatch.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+
+import pyinotify
+import argparse
+from subprocess import call
+
+parser = argparse.ArgumentParser(
+    description='''Will watch files in the specified 
+    directory for updates and run compile commands 
+    based on their extensions''')
+parser.add_argument('-r', '--recursive', action="store_true",
+        default = False,
+        help='Will recursively watch the dir and its subdirs')
+parser.add_argument('directory', type=str,
+        help='The directory to watch')
+args = parser.parse_args()
+
+wm = pyinotify.WatchManager()
+# Handle updates for on create and on modify
+mask = pyinotify.IN_CREATE | pyinotify.IN_MODIFY
+
+class EventHandler(pyinotify.ProcessEvent):
+    ext_map = {
+        '.tex': ['pdflatex'],
+        '.less': ['lessc'],
+        '.scala': ['scala'],
+        '.sass': ['sass']
+    }
+
+    def process_IN_CREATE(self, event):
+        print "Creating: ", event.pathname
+
+    def process_IN_MODIFY(self, event):
+        print "Updating: ", event.pathname
+        self.run_command(event.path, event.name)
+
+    def run_command(self, file_path, file_name):
+        # Find the extension
+        ext_index  = file_name.rfind('.')
+        ext = file_name[ext_index:]
+        # Determine if this ext is in the map
+        if ext in self.ext_map:
+            print "Running Command: " + self.ext_map[ext][0] + " " + file_path + "/" +  file_name
+            call(self.ext_map[ext][0] + " " + file_path + "/" + file_name, shell=True)
+        else:
+            print "Ext not found"
+
+handler = EventHandler()
+notifier = pyinotify.Notifier(wm, handler)
+wdd = wm.add_watch(args.directory, mask, rec=args.recursive)
+
+notifier.loop()
